87ef64b93d7773357336e7b9060054a4e12e1fca
David Hsu <exutechnology@gmail.com>
2025-10-09 23:47:55 -0700

    Add qumater/__init__.py
    Initialize the QuMater package with high-level namespaces.

---
+++ qumater/__init__.py
@@ -0,0 +1,12 @@
+"""Top level package for QuMater.
+
+This module exposes high level namespaces for materials and qsim. Most
+functionality is available from subpackages and the CLI; see
+`help(qumater)` for details.
+"""
+
+from importlib import import_module as _import_module
+
+__all__ = ['materials', 'qsim']
+materials = _import_module('qumater.materials')
+qsim = _import_module('qumater.qsim')
